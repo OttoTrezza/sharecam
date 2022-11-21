f619beaa63acebc42fd62ec0dd85c6ac6b78fec0
OttoTrezza <ignacio.trezza@outlook.com>
2022-11-21 02:01:33 -0300

    delete googlesignin

--- routes/login.js
+++ routes/login.js
@@ -7,11 +7,7 @@ require('../config/config');
 var app = express();
 var Usuario = require('../models/usuario');
 
-var GoogleAuth = require('google-auth-library');
-var auth = new GoogleAuth;
 
-const GOOGLE_CLIENT_ID = require('../config/config').GOOGLE_CLIENT_ID;
-const GOOGLE_SECRET = require('../config/config').GOOGLE_SECRET;
 
 var mdAutenticacion = require('../middlewares/autenticacion');
 // ==========================================
@@ -31,116 +27,7 @@ app.get('/renuevatoken', mdAutenticacion.verificaToken, (req, res) => {
 // ==========================================
 //  Autenticación De Google
 // ==========================================
-app.post('/google', (req, res) => {
 
-    var token = req.body.token || 'XXX';
-
-
-    var client = new auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_SECRET, '');
-
-    client.verifyIdToken(
-        token,
-        GOOGLE_CLIENT_ID,
-        // Or, if multiple clients access the backend:
-        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3],
-        function(e, login) {
-
-            if (e) {
-                return res.status(400).json({
-                    ok: true,
-                    mensaje: 'Token no válido',
-                    errors: e
-                });
-            }
-
-
-            var payload = login.getPayload();
-            var userid = payload['sub'];
-            // If request specified a G Suite domain:
-            //var domain = payload['hd'];
-
-            Usuario.findOne({ email: payload.email }, (err, usuario) => {
-
-                if (err) {
-                    return res.status(500).json({
-                        ok: true,
-                        mensaje: 'Error al buscar usuario - login',
-                        errors: err
-                    });
-                }
-
-                if (usuario) {
-
-                    if (usuario.google === false) {
-                        return res.status(400).json({
-                            ok: true,
-                            mensaje: 'Debe de usar su autenticación normal'
-                        });
-                    } else {
-
-                        usuario.password = ':)';
-
-                        var token = jwt.sign({ usuario: usuario }, SEED, { expiresIn: 14400 }); // 4 horas
-
-                        res.status(200).json({
-                            ok: true,
-                            usuario: usuario,
-                            token: token,
-                            id: usuario._id,
-                            menu: obtenerMenu(usuario.role)
-                        });
-
-                    }
-
-                    // Si el usuario no existe por correo
-                } else {
-
-                    let usuario = new Usuario();
-
-
-                    usuario.nombre = payload.name;
-                    usuario.email = payload.email;
-                    usuario.password = ':)';
-                    usuario.img = payload.picture;
-                    usuario.google = true;
-
-                    usuario.save((err, usuarioDB) => {
-
-                        if (err) {
-                            return res.status(500).json({
-                                ok: true,
-                                mensaje: 'Error al crear usuario - google',
-                                errors: err
-                            });
-                        }
-
-
-                        var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
-
-                        res.status(200).json({
-                            ok: true,
-                            usuario: usuarioDB,
-                            token: token,
-                            id: usuarioDB._id,
-                            sala: usuarioDB.sala,
-                            menu: obtenerMenu(usuarioDB.role)
-
-                        });
-
-                    });
-
-                }
-
-
-            });
-
-
-        });
-
-
-
-
-});
 
 // ==========================================
 //  Autenticación normal
